6750d3e337135cb9e58e2acfd84830671404c4ac
paul-madut <paul.madut@mths.ca>
2022-11-13 17:56:20 -0500

    Finished About Section

---
+++ app.js
@@ -0,0 +1,15 @@
+var tabLinks = document.getElementsByClassName("tab__links");
+var tabContents = document.getElementsByClassName("tab__contents");
+
+function opentab(tabName){
+    for(tablink of tabLinks){
+        tablink.classList.remove("active__link");
+    }
+    for(tabContent of tabContents){
+        tabContent.classList.remove("active__tab")
+    }
+
+    event.currentTarget.classList.add("active__link");
+    document.getElementById(tabName).classList.add("active__tab")
+    
+}
