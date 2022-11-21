45c72c2065f00e664984855ae8ede4ed33c8a7b7
paul-madut <paul.madut@mths.ca>
2022-11-21 16:09:49 -0500

    Form works

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import Typed from 'typed.js'
+
 
 var tabLinks = document.getElementsByClassName("tab__links");
 var tabContents = document.getElementsByClassName("tab__contents");
@@ -18,9 +18,3 @@ function opentab(tabName){
 
 
     
-    var typed = new Typed(".hero__highlight",{
-    strings:["Paul","Paul Madut","Paul The Programmer"],
-    typeSpeed: 150,
-    backSpeed: 150,
-    loop: true
-})
